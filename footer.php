9880acb204a76c64079ee1964f881303673140e3
Angel-smith974 <elfedelanuit974@hotmail.fr>
2019-09-30 15:55:26 +0400

    premier commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+<h3>Ceci est le footer</h3>
+<ul>
+    <li>contact</li>
+    <li>formulaire</li>
+    <li>Quelque chose</li>
+</ul>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>les inclusions</title>
+</head>
+<body>
+<?php
+include("navbar.php");
+?>
+    <h1>Gros titre</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea vel veniam eveniet sed deserunt vitae quaerat unde corporis nam animi numquam maiores amet facere, quos, expedita quia minima, debitis ut?</p>
+
+    <?php
+include("footer.php");
+?>
+</body>
+</html>
